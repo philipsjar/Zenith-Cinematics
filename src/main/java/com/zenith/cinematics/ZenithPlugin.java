ab68ec92a66dc0c3564f642cb0b386db3cc6ce6f
Philips <broker51@proton.me>
2026-02-14 16:30:58 +0000

    Update ZenithPlugin.java

--- src/main/java/com/zenith/cinematics/ZenithPlugin.java
+++ src/main/java/com/zenith/cinematics/ZenithPlugin.java
@@ -1,16 +1,12 @@
 package com.zenith.cinematics;
-
+import com.zenith.cinematics.camera.CameraCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-
 public class ZenithPlugin extends JavaPlugin {
     private static ZenithPlugin instance;
-
     @Override
     public void onEnable() {
         instance = this;
+        getCommand("cinematic").setExecutor(new CameraCommand());
     }
-
-    public static ZenithPlugin getInstance() {
-        return instance;
-    }
+    public static ZenithPlugin getInstance() { return instance; }
 }
